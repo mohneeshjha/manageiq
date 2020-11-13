8e28d12920b9d308e2292e447b8b4f0eb7a8eb3c
odidev <odidev@puresoftware.com>
2020-11-13 11:33:05 +0530

    Arm64 Support For Travis-Ci
    Signed-off-by: odidev <odidev@puresoftware.com>

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -v
 
+sudo sed -i 's/127.0.1.1/127.0.0.1/' /etc/hosts
+git config --global user.name "ManageIQ"
+git config --global user.email "contact@manageiq.org"
 source $TRAVIS_BUILD_DIR/bin/ci/setup_vmdb_configs.sh
 source $TRAVIS_BUILD_DIR/bin/ci/setup_ruby_env.sh
 
